25aa9a256d60237d8a2059c1e3f85bdefe11b551
Philip Ezhukattil <philipe@phys.ethz.ch>
2011-11-29 15:50:45 +0100

    lvm2fs with ajax

--- index.pl
+++ index.pl
@@ -22,110 +22,133 @@ my $CHARTSPERLINE = 3;	#number of LV charts in one line
 
 my (%lvm, $UNIT);
 my ($markup, $javascript);
-my $warnings;
+my $warnings = '';
 my $format = new Number::Format(-thousands_sep   => '\'', -decimal_point   => '.');
 
-print "Content-type:text/html\r\n\r\n";
+my $option = $ENV{'QUERY_STRING'};
+my $counter;
 
-foreach my $server (@servers) {
-	my $json_text;
-	if (!($json_text = `ssh -o IdentitiesOnly=yes -i ~/.ssh/remotesshwrapper root\@$server /usr/local/bin/remotesshwrapper lvm2.pl`)) {
-		$warnings .= "could not fetch JSON from server $server!<br />\n";
-	} else {
-		my $json = JSON->new->allow_nonref;
-		my $lvm;
-		if (!($lvm = $json->decode($json_text))) {
-			$warnings .= "could not get LVM data from server $server<br />\n"; next;
-		}
-		%lvm = %$lvm;
-
-		my $numLVs = 0;	#count LV
-		foreach my $vg (keys %{$lvm{'vgs'}}) {
-			foreach my $lv (keys %{$lvm{$vg}{'lvs'}}) {
-				$numLVs++;
-			}
-		}
-
-		my $serverID = $server;
-		$serverID =~ s/-/_/g;	#Google chart API needs underscores
-		$UNIT = $lvm{'unit'};	#TB or GB comes from the JSON
-
-		if ($lvm{'warning'}) {
-			$warnings .= "Host $server: ".$lvm{'warning'}."<br />\n";
-		}
-		next unless (keys %{$lvm{'vgs'}});	#skip host if no vgs present
-
-		my $PVFSLevel = $lvm{'FSLevel'};	#get PV data
-		my $PVInFS = $lvm{'inFS'} - $PVFSLevel;
-		my $PVInLV = $lvm{'inLV'};
-		my $PVInVG = $lvm{'inVG'};
-		my $PVInPV = $lvm{'inPV'};
-		my $PVSize = $lvm{'size'};
-		$PVSize = $format->format_number($PVSize);
-		$PVSize =~ s/'/\\'/;
-		$javascript .= pvData($serverID, $PVFSLevel, $PVInFS, $PVInLV, $PVInVG, $PVInPV, $PVSize, $UNIT);
-
-		my ($vgRows, $lvRows, $orgRows);
-		my $lvGrpOrg = 0;
-		my $lvGrpChart = 0;
-		my $orgChart .= "<div class=\"orgchart\" id=\"org_chart_${serverID}_$lvGrpOrg\"></div>";
-		foreach my $vg (sort keys %{$lvm{'vgs'}}) {
-			my $VGFSLevel = $lvm{'pv'}{$vg}{'FSLevel'};	#get VG data
-			my $VGInFS = $lvm{'pv'}{$vg}{'inFS'} - $VGFSLevel;
-			my $VGInLV = $lvm{'pv'}{$vg}{'inLV'};
-			my $VGInVG = $lvm{'pv'}{$vg}{'inVG'};
-			my $VGSize = $lvm{'pv'}{$vg}{'size'};
-			$VGSize = $format->format_number($VGSize);
-			$VGSize =~ s/'/\\'/;
-
-			$vgRows .= " {c:[{v:'$vg'},{v:$VGFSLevel},{v:$VGInFS},{v:$VGInLV},{v:$VGInVG}]},";
-			$orgRows .= "{c:[{v:'$vg',f:'$vg<div class=\"parent\">$VGSize$UNIT</div>'}, '','VG size']},";
-
-			my $lvcount = 0;
-			foreach my $lv (sort keys %{$lvm{$vg}{'lvs'}}) {
-				my $LVFSLevel = $lvm{'pv'}{$vg}{$lv}{'FSLevel'};	#get LV data
-				my $LVInFS = $lvm{'pv'}{$vg}{$lv}{'inFS'} - $LVFSLevel;
-				my $LVInLV = $lvm{'pv'}{$vg}{$lv}{'inLV'};
-				my $LVSize = $lvm{'pv'}{$vg}{$lv}{'size'};
-				$LVSize = $format->format_number($LVSize);
-				$LVSize =~ s/'/\\'/;
-
-				if ($lvcount && !($lvcount % $LVSPERCHART)) {	#if LV chart is full, create a new one
-					$javascript .= lvData("${serverID}_$lvGrpChart", $lvRows);
-					$lvGrpChart = int($lvcount / $LVSPERCHART);
-					$lvRows = '';
-				}
-
-				if ($lvcount && !($lvcount % $ORGSPERCHART)) {	#if org chart is full, create a new one
-					$javascript .= orgChart("${serverID}_$lvGrpOrg", $orgRows);
-					$lvGrpOrg = int($lvcount / $ORGSPERCHART);
-					$orgChart .= "<br /><br /><div class=\"orgchart\" id=\"org_chart_${serverID}_$lvGrpOrg\"></div>\n";
-					$orgRows = "{c:[{v:'$vg',f:'$vg<div class=\"parent\">$VGSize$UNIT</div>'}, '','VG size']},";
-				}
-				$lvcount++;
-
-				$lvRows .= "{c:[{v:'$lv'},{v:$LVFSLevel},{v:$LVInFS},{v:$LVInLV}]},";	#fill LV and org chart data
-				$orgRows .= "{c:[{v:'$lv<div class=\"child\">$LVSize$UNIT</div>'},{v:'$vg'},'LV size']},";
-			}
-		}
-		chop $vgRows;	#trim last comma
-		chop $lvRows;
-		chop $orgRows;
-
-		$javascript .= vgData($serverID, $vgRows);
-		$javascript .= lvData("${serverID}_$lvGrpChart", $lvRows);
-		$javascript .= orgChart("${serverID}_$lvGrpOrg", $orgRows);
-		$markup .= chartTable($server, $serverID, $orgChart, $numLVs);
-	}
+#verification of the querystring
+if ($option eq 'data'){
+	print "Content-type:text/plain\r\n\r\n";
+	$| = 1;		# Flush output continuously
+	&getdata();
+}else{
+	print "Content-type:text/html\r\n\r\n";
+	print html();
 }
 
-print html();
+
+
+sub getdata {
+	foreach my $server (@servers) {
+		my $json_text;
+		if (!($json_text = `ssh -o IdentitiesOnly=yes -i ~/.ssh/remotesshwrapper root\@$server /usr/local/bin/remotesshwrapper lvm2.pl`)) {
+			$warnings .= "could not fetch JSON from server $server! $!<br />\n";
+		} else {
+			my $json = JSON->new->allow_nonref;
+			my $lvm;
+			if (!($lvm = $json->decode($json_text))) {
+				$warnings .= "could not get LVM data from server $server<br />\n"; next;
+			}
+			%lvm = %$lvm;
+
+			my $numLVs = 0;	#count LV
+			foreach my $vg (keys %{$lvm{'vgs'}}) {
+				foreach my $lv (keys %{$lvm{$vg}{'lvs'}}) {
+					$numLVs++;
+				}
+			}
+
+			my $serverID = $server;
+			$serverID =~ s/-/_/g;	#Google chart API needs underscores
+			$UNIT = $lvm{'unit'};	#TB or GB comes from the JSON
+
+			if ($lvm{'warning'}) {
+				$warnings .= "Host $server: ".$lvm{'warning'}."<br />\n";
+			}
+			next unless (keys %{$lvm{'vgs'}});	#skip host if no vgs present
+
+			my $PVFSLevel = $lvm{'FSLevel'};	#get PV data
+			my $PVInFS = $lvm{'inFS'} - $PVFSLevel;
+			my $PVInLV = $lvm{'inLV'};
+			my $PVInVG = $lvm{'inVG'};
+			my $PVInPV = $lvm{'inPV'};
+			my $PVSize = $lvm{'size'};
+			$PVSize = $format->format_number($PVSize);
+			$PVSize =~ s/'/\\'/;
+			$javascript .= pvData($serverID, $PVFSLevel, $PVInFS, $PVInLV, $PVInVG, $PVInPV, $PVSize, $UNIT);
+
+			my ($vgRows, $lvRows, $orgRows);
+			my $lvGrpOrg = 0;
+			my $lvGrpChart = 0;
+			my $orgChart .= "<div class=\"orgchart\" id=\"org_chart_${serverID}_$lvGrpOrg\"></div>";
+			foreach my $vg (sort keys %{$lvm{'vgs'}}) {
+				my $VGFSLevel = $lvm{'pv'}{$vg}{'FSLevel'};	#get VG data
+				my $VGInFS = $lvm{'pv'}{$vg}{'inFS'} - $VGFSLevel;
+				my $VGInLV = $lvm{'pv'}{$vg}{'inLV'};
+				my $VGInVG = $lvm{'pv'}{$vg}{'inVG'};
+				my $VGSize = $lvm{'pv'}{$vg}{'size'};
+				$VGSize = $format->format_number($VGSize);
+				$VGSize =~ s/'/\\'/;
+
+				$vgRows .= " {c:[{v:'$vg'},{v:$VGFSLevel},{v:$VGInFS},{v:$VGInLV},{v:$VGInVG}]},";
+				$orgRows .= "{c:[{v:'$vg',f:'$vg<div class=\"parent\">$VGSize$UNIT</div>'}, '','VG size']},";
+
+				my $lvcount = 0;
+				foreach my $lv (sort keys %{$lvm{$vg}{'lvs'}}) {
+					my $LVFSLevel = $lvm{'pv'}{$vg}{$lv}{'FSLevel'};	#get LV data
+					my $LVInFS = $lvm{'pv'}{$vg}{$lv}{'inFS'} - $LVFSLevel;
+					my $LVInLV = $lvm{'pv'}{$vg}{$lv}{'inLV'};
+					my $LVSize = $lvm{'pv'}{$vg}{$lv}{'size'};
+					$LVSize = $format->format_number($LVSize);
+					$LVSize =~ s/'/\\'/;
+
+					if ($lvcount && !($lvcount % $LVSPERCHART)) {	#if LV chart is full, create a new one
+						$javascript .= lvData("${serverID}_$lvGrpChart", $lvRows);
+						$lvGrpChart = int($lvcount / $LVSPERCHART);
+						$lvRows = '';
+					}	
+
+					if ($lvcount && !($lvcount % $ORGSPERCHART)) {	#if org chart is full, create a new one
+						$javascript .= orgChart("${serverID}_$lvGrpOrg", $orgRows);
+						$lvGrpOrg = int($lvcount / $ORGSPERCHART);
+						$orgChart .= "<br /><br /><div class=\"orgchart\" id=\"org_chart_${serverID}_$lvGrpOrg\"></div>\n";
+						$orgRows = "{c:[{v:'$vg',f:'$vg<div class=\"parent\">$VGSize$UNIT</div>'}, '','VG size']},";
+					}
+					$lvcount++;
+
+					$lvRows .= "{c:[{v:'$lv'},{v:$LVFSLevel},{v:$LVInFS},{v:$LVInLV}]},";	#fill LV and org chart data
+					$orgRows .= "{c:[{v:'$lv<div class=\"child\">$LVSize$UNIT</div>'},{v:'$vg'},'LV size']},";
+				}
+			}
+			chop $vgRows;	#trim last comma
+			chop $lvRows;
+			chop $orgRows;
+
+			$javascript .= vgData($serverID, $vgRows);
+			$javascript .= lvData("${serverID}_$lvGrpChart", $lvRows);
+			$javascript .= orgChart("${serverID}_$lvGrpOrg", $orgRows);
+			$markup .= chartTable($server, $serverID, $orgChart, $numLVs);
+
+			#Print information about this server
+                        print $javascript;
+			print "ENDOFELEMENT";
+			print $markup;
+			print "ENDOFELEMENT";
+			print $warnings;
+			print "ENDOFSERVER";
+
+			#Clear variables for next iteration
+			$javascript = $markup = $warnings = "";
+		}
+	}
+}
 
 
 #----------------
 sub html {	#HTML template
 	my $css = css();
-	$warnings = "<h2>Warnings</h2>".$warnings if ($warnings);
 	return <<EOF;
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml"> 
@@ -137,28 +160,68 @@ sub html {	#HTML template
     <style type="text/css">
     	$css
     </style>
- 
-    <script type="text/javascript" src="http://www.google.com/jsapi"></script> 
+    <script type="text/javascript" src="http://www.google.com/jsapi"></script>     
     <script type="text/javascript"> 
-      google.load('visualization', '1', {packages: ['corechart']});
-      google.load('visualization', '1', {packages:['orgchart']});
-    </script> 
- 
-    <script type="text/javascript"> 
-    google.setOnLoadCallback(drawVisualization);
-      function drawVisualization() {
-	$javascript
-      }
-    </script> 
-  </head> 
-  
-  <body> 
+	       google.load('visualization', '1', {packages: ['corechart']});
+	       google.load('visualization', '1', {packages:['orgchart']});
+     		
+    var numberOfServersDisplayed = 0;	
+    var req = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP"); // Create Ajax request object
+	
+	function init(){
+		//Use ajax to load server data
+		ajaxLoad("https://admin.phys.ethz.ch/lvm2FS/index.pl?data", ajaxOnResult);
+	}
+        
+	function ajaxLoad(url, callback){
+		req.open("GET", url, true);
+		req.send(null);								
+		
+		req.onreadystatechange = callback;					//Start ajaxOnResult function after the stream
+		window.setTimeout( function() {ajaxOnProgress(req);},500);
+	}
+	
+	function ajaxOnProgress(req) {
+		if (req.readyState !=4){						//while the data are received
+			var msg = document.getElementById("message");		
+			msg.innerHTML = 'Loading Server Data...';
+	
+			ajaxDisplayServer();	
+			window.setTimeout( function() {ajaxOnProgress(req);},500);  		
+		}
+	}
+
+	function ajaxDisplayServer(){		
+		if(req.responseText.match(/ENDOFSERVER/g)){								
+			var server = req.responseText.split(/ENDOFSERVER/g);		//Split the Stream in Server                           
+			var numberOfServersReceived = server.length -1;			//the number of servers in the array
+
+			for(i=numberOfServersDisplayed; i< numberOfServersReceived; i++){			//check if all the server data will be displayed
+				var serverdata = server[i].split(/ENDOFELEMENT/g);	//Split by elements(js/markup/wanings)
+				document.getElementById('js').innerHTML += serverdata[0];			//insert data
+				document.getElementById('table').innerHTML += serverdata[1];
+				document.getElementById('warnings').innerHTML += serverdata[2];                                      
+				eval(document.getElementById('js').innerHTML);
+				numberOfServersDisplayed++;				
+			}		
+		}
+	}	
+
+	function ajaxOnResult(evt){
+		if ((evt.currentTarget.readyState == 4) && (evt.currentTarget.status == 200 || evt.currentTarget.status == 0)) {
+			ajaxDisplayServer();									//double check if all the server data was displayed					
+			document.getElementById("message").style.visibility = 'hidden';		
+			}
+	}
+</script>
+ </head> 
   <div class="header">LVMchart - LVM Monitoring</div> 
-      <table align="center"> 
-    	$markup
-    </table> 
-	$warnings
-  </body> 
+  <body onload="init();"> 
+	<div id="message"></div>
+	<script type="text/javascript" id="js"></script>
+        <table align="center" id="table"></table>
+	<div id="warnings"></div>
+</body> 
 </html> 
 EOF
 }
@@ -339,7 +402,16 @@ sub css {
 	font-weight: bold;
 	text-align: center;
 }
+#warnings {
+	color: red;
+}
 
+#message {
+	line-height: 6em;
+	color: green; 
+	font-weight: bold;
+	text-align: center;
+}
 body {
 	background: #C9D5E5;
 	font-family: Arial;
